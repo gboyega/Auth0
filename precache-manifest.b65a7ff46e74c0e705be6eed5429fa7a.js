23a06b14e74543ede7b5e77169faab50c5bc4892
gboyega <jaiyeolagboyega@gmail.com>
2019-11-17 16:32:14 +0100

    Updates

---
+++ precache-manifest.b65a7ff46e74c0e705be6eed5429fa7a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ba49cbbeebfb20113e7f0cf377247c",
+    "url": "/Auth0/index.html"
+  },
+  {
+    "revision": "ea00713fabfeae17a2d1",
+    "url": "/Auth0/static/js/2.80855dee.chunk.js"
+  },
+  {
+    "revision": "2089b327332f6d802f53",
+    "url": "/Auth0/static/js/main.a8cd9485.chunk.js"
+  },
+  {
+    "revision": "9528834a89ef48449419",
+    "url": "/Auth0/static/js/runtime-main.bac4d865.js"
+  }
+]);
